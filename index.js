6bf6c541f36be39e3eae0f6c8f1542271f47ff76
Siranjeevi <siranjeevi0619@gmail.com>
2025-02-22 09:17:01 +0530

    removed comments

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
-// Creating multilevel array
 const arr = [
   ["1", "2"],
   ["3", "4", "5", ["6"], "7"],
 ];
-
-// Flat the multilevel array
 const flatArr = arr.flat(Infinity);
 console.log(flatArr);
